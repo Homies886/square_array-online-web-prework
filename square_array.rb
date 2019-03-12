71ecc4f1153af2f619b0c987475b2e98c833a782
fortunate-partial-4781 <niko876@hotmail.com>
2019-03-12 16:23:24 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,12 @@
 def square_array(array)
+<<<<<<< HEAD
   squared = []
   array.each { |each_number| squared << each_number ** 2 }
   squared
+=======
+  array.each do |square|
+    array << square ** 2
+  end
+  array.each
+>>>>>>> 991f46f0f2fc2706e38710a8d51a579793ac5a04
 end
